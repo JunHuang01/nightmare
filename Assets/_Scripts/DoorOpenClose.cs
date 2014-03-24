9d70abc3d095bb88bd174e4835859c96fc19d2a8
Jun Huang <mightybeesun@gmail.com>
2014-03-24 16:38:56 -0400

    Modified door to be more responsive Changed the key down to be detected at FixedUpdate()

--- Assets/_Scripts/DoorOpenClose.cs
+++ Assets/_Scripts/DoorOpenClose.cs
@@ -8,40 +8,40 @@ public class DoorOpenClose : MonoBehaviour {
     private bool bIsDoorOpen = false;
 
     Vector3 closePosition;
+    Vector3 openPosition;
     Vector3 newPosition;
 
     void OnAwake() { 
          newPosition = transform.eulerAngles;
+
     }
 
     void Update() {
         DoorPositionChange();
     }
     void FixedUpdate() {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            bIsDoorOpen = !bIsDoorOpen;
+        }   
     }
 
     void DoorPositionChange() {
-
         //define open and close position
-        Vector3 openPosition = new Vector3(transform.eulerAngles.x, 90.0f, transform.eulerAngles.z);
-        Vector3 closePosition = new Vector3(transform.eulerAngles.x, 0.0f, transform.eulerAngles.z);
+        openPosition = new Vector3(transform.eulerAngles.x, 90.0f, transform.eulerAngles.z);
+        closePosition = new Vector3(transform.eulerAngles.x, 0.0f, transform.eulerAngles.z);
 
+        
 
         //when press E key, we define what position the door should go to.
-        if (Input.GetKeyDown(KeyCode.E)) {
-            if (bIsDoorOpen)
-            {
-                newPosition = closePosition;
-            }
-            else
-            {
-                newPosition = openPosition;
-            }
+        if (bIsDoorOpen)
+        {
+            newPosition = openPosition;
         }
-
-        //flip the boolean for door state
-        bIsDoorOpen = !bIsDoorOpen;
+        else
+        {
+            newPosition = closePosition;
+        }
 
         //finish the door open and close operation over time
         transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, newPosition, smooth * Time.deltaTime);
