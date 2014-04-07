6ecc4520ec19edf835c9924cf2c0dd3b656f0367
Jun Huang <mightybeesun@gmail.com>
2014-04-07 06:56:54 -0400

    Implemented the basic version of a Sweater Enemy with no logic The player detection is in place now, finish this by adding game logic to the monster after sighting player in onPlayerSighted() function.

---
+++ Assets/_Scripts/SweaterEnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SweaterEnemyAI : MonoBehaviour {
+    private bool isPlayerSighted;
+    void Awake() {
+        isPlayerSighted = false;
+    }
+
+    void OnTriggerEnter() {
+        isPlayerSighted = true;
+    }
+
+    void OnTriggerExit() {
+        isPlayerSighted = false;
+    }
+
+    void Update() {
+        OnPlayerSighted();
+    }
+
+    //ToDo: rewrite this function for proper Enemy Response
+    void OnPlayerSighted() {
+
+        //Testing to see if player enters in sight testing cube change color
+        if (isPlayerSighted)
+        {
+            renderer.material.color = Color.red;
+        }
+        else
+        {
+            renderer.material.color = Color.yellow;
+        }
+    }
+}
