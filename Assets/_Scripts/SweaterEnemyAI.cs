ead15cbfd40584146ce60f804f7b03fe7986d51c
Jun Huang <mightybeesun@gmail.com>
2014-04-07 07:06:13 -0400

    Added player sighted pos. Now this script will keep track of where the player is when the player is in sight of the monster.

--- Assets/_Scripts/SweaterEnemyAI.cs
+++ Assets/_Scripts/SweaterEnemyAI.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class SweaterEnemyAI : MonoBehaviour {
     private bool isPlayerSighted;
+    private Vector3 playerLastSightPos;
+    private Vector3 playerNotSightedPos;
+
+    private GameObject player;
     void Awake() {
         isPlayerSighted = false;
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        playerNotSightedPos = new Vector3(99999f, 99999f, 99999f);
     }
 
     void OnTriggerEnter() {
@@ -25,11 +31,13 @@ public class SweaterEnemyAI : MonoBehaviour {
         //Testing to see if player enters in sight testing cube change color
         if (isPlayerSighted)
         {
+            playerLastSightPos = player.transform.position;
             renderer.material.color = Color.red;
         }
         else
         {
             renderer.material.color = Color.yellow;
+            playerLastSightPos = playerNotSightedPos;
         }
     }
 }
