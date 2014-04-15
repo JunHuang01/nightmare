4b686efb89795dc07321d366a514cab3d56ccf51
Jason Hettmansperger <jjbeanz@Jasons-MacBook-Pro-7.local>
2014-04-14 20:09:01 -0400

    New character, new bullet, added monsters, monster movement

--- Assets/_Scripts/enemymovement.cs
+++ Assets/_Scripts/enemymovement.cs
@@ -3,23 +3,22 @@ using System.Collections;
 
 public class enemymovement : MonoBehaviour {
 
-	public float speed = 1f;
-	public Vector3 movedir = Vector3.zero;
-	// Use this for initialization
-	void Start () {
+	public float speed = 1;
+	public Vector3 movedir = Vector3.back;
 	
+	void Start () {
+		movedir = Vector3.forward;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if (transform.position.z > 3f && movedir != Vector3.back) {
+	void Update() {
+		if (transform.position.z > 5f && movedir != Vector3.back) {
 			movedir = Vector3.back;
 			transform.Rotate(0,180,0);
-	}
+		}
 		if (transform.position.z < 0f && movedir != Vector3.forward) {
 			movedir = Vector3.forward;
-			//transform.Rotate(0,180,0);
+			transform.Rotate(0,180,0);
 		}
-		transform.Translate (movedir * speed * Time.deltaTime);
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
 	}
 }
