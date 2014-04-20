1de5310a29fec9f3cb1bceadc70e4eb1010fc7a6
Jun Huang <mightybeesun@gmail.com>
2014-04-20 00:03:06 -0400

    Fixed the GameController Fixed the candle enemy attacking and moving transition problem Refactored the animators tweaked the player's setting so hopefully it is not going to get bumped up by other object anymore.

---
+++ Assets/_Scripts/HashIDs.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+    public int candleAttack;
+
+    void Awake() {
+        candleAttack = Animator.StringToHash("CandleAttack");
+    }
+}
