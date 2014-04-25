80736a996bde700246251bbd17a0ee2e58c3b239
Jun Huang <mightybeesun@gmail.com>
2014-04-24 20:33:32 -0400

    refactored door so that enemy line of sight is not raycasting on the door activation area

---
+++ Assets/_Scripts/isPlayerNear.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isPlayerNear : MonoBehaviour {
+
+    private DoorOpenClose doorScript;
+    private GameObject player;
+    void Awake() {
+        doorScript = transform.parent.GetComponent<DoorOpenClose>();
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        //when player enters activation zone, player is near
+        if (other.gameObject == player) {
+            doorScript.isPlayerNear = true;
+        }    
+    }
+
+    void OnTriggerExit(Collider other) {
+        //when player exits activation zone, player is not near
+        if (other.gameObject == player) {
+            doorScript.isPlayerNear = false;
+        }
+    }
+
+}
