444b808c9c8b93489cde995e0992530d4abe17fc
Jun Huang <mightybeesun@gmail.com>
2014-04-25 19:35:22 -0400

    Bullets water particle added, yeah bitch.

--- Assets/_Scripts/GameLogic.cs
+++ Assets/_Scripts/GameLogic.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class GameLogic : MonoBehaviour {
 
     void Awake() {
+        Screen.lockCursor = true;
         //Application.LoadLevel("nightmare");
     }
 	// Use this for initialization
 	void Start () {
 	    
 	}
+
+    void OnApplicationFocus(bool focused)
+    {
+        Screen.lockCursor = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
