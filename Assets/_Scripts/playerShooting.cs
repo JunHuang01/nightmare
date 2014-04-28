4625c62e29522f1676c25e5f5cdd383920945840
Jun Huang <mightybeesun@gmail.com>
2014-04-27 22:26:15 -0400

    fixed the bullet sound issue by moving bullets sound from bullet to char

--- Assets/_Scripts/playerShooting.cs
+++ Assets/_Scripts/playerShooting.cs
@@ -5,9 +5,11 @@ public class playerShooting : MonoBehaviour {
     public float nextFire;
     public GameObject waterBullet; //bullet object that will be spawned
     public Transform shotSpawn; //position of where the bullets spawned
+
+
 	// Use this for initialization
 	void Start () {
-	
+ 
 	}
 	
 	// Update is called once per frame
@@ -15,8 +17,12 @@ public class playerShooting : MonoBehaviour {
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
 
+
             // keep track of when can you fire the next bullet 
             nextFire = Time.time + GlobalConstant.nextPlayerFireRate;
+
+            //play the shooting sound
+            audio.Play();
 
             //spawn the bullet
             Instantiate(waterBullet, shotSpawn.position, shotSpawn.rotation);
